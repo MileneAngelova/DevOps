aa876d6e82818bbacbe2072b132a975f3b878f18
Milena Angelova <milene.angelova@gmail.com>
2024-09-24 19:58:04 +0100

    api tests added

--- SoftwareEngineering/AutomatedTesting/Books/server.js
+++ SoftwareEngineering/AutomatedTesting/Books/server.js
@@ -19,7 +19,7 @@ server.post('/books', (req, res) => {
 server.get('/books/:id', (req, res) => {
     const book = books.find(b => b.id === req.params.id);
     if (book) res.json(book);
-    else res.status(404).json({message: 'Book not found'});
+    else res.status(404).json({ message: 'Book not found' });
 });
 
 server.put('/books/:id', (req, res) => {
@@ -28,7 +28,7 @@ server.put('/books/:id', (req, res) => {
         books[bookIndex] = req.body;
         res.json(books[bookIndex]);
     } else {
-        res.status(404).json({message: 'Book not found'});
+        res.status(404).json({ message: 'Book not found' });
     }
 });
 
@@ -38,11 +38,10 @@ server.delete('/books/:id', (req, res) => {
         books.splice(bookIndex, 1);
         res.status(204).end();
     } else {
-        res.status(404).json({message: 'Book not found'});
+        res.status(404).json({ message: 'Book not found' });
     }
 });
 
-if (require.main === module) {
-    server.listen(3000, () => console.log('Server is up and running'));
-}
+server.listen(3000, () => console.log('Server is up and running'));
+
 module.exports = server; // Exporting for testing
