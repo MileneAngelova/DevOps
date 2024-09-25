092c6d5e0a2e88acaf217dad29d31c38fa07aa5d
Milena Angelova <milene.angelova@gmail.com>
2024-09-25 17:20:52 +0100

    Some changes

---
+++ SoftwareEngineering/AutomatedTesting/Books/api.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./server');
+const { describe } = require('mocha');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Books', () => {
+    let bookId;
+    it('should POST a book', (done) => {
+        const book = {id: "1", title: "Test Book", author: "Test Author"}
+        chai.request(server)
+        .post('/books')
+        .send(book)
+        .end((err, res) => {
+            expect(res).to.have.status(201);
+            expect(res.body).to.be.a('object');
+            expect(res.body).to.have.property('id');
+            expect(res.body).to.have.property('title');
+            expect(res.body).to.have.property('author');
+            book.id = res.body.id;
+            console.log('response: ', res.body);
+            done();
+        })
+    })
+});
